63a42eb06d4eab98aa5c674dc1f3960525f98be6
anaghpro <84767861+anaghpro@users.noreply.github.com>
2021-09-13 21:02:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach(camera);
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_img" src="' + data_uri + '"/>';
+    });
+}
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/0d69cTYp9/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+function check(){
+    img = document.getElementById("captured_img");
+    classifier.classify(img,show_result);
+}
+function show_result(error,result){
+    if(error){console.error(error);}
+    else{console.log(result);}
+    document.getElementById("result_object_name").innerHTML=result[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=result[0].confidence.toFixed(3);
+}
